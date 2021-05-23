09749f264c51f42ae73eca96480cb471eda11f54
ahaibof <bszy4593@163.com>
2021-05-23 10:56:51 +0800

    Logging tweaks; enaml note; import change

--- profile_xf03id/startup/00-startup.py
+++ profile_xf03id/startup/00-startup.py
@@ -1,3 +1,24 @@
 import logging
+
 session_mgr._logger.setLevel(logging.INFO)
-from ophyd.userapi import *
+# session_mgr._logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stderr)
+fmt = logging.Formatter("%(asctime)-15s [%(name)5s:%(levelname)s] %(message)s")
+handler.setFormatter(fmt)
+logging.getLogger('hxntools').addHandler(handler)
+logging.getLogger('hxnfly').addHandler(handler)
+logging.getLogger('hxnfly').setLevel(logging.INFO)
+logging.getLogger('ppmac').setLevel(logging.INFO)
+
+try:
+    from ophyd.commands import *
+except ImportError:
+    from ophyd.userapi import *
+
+# NOTE: may need to have enaml.qt.prepare_pyqt() in IPython configuration before
+# anything else is loaded...
+from dataportal import (DataBroker as db,
+                        StepScan as ss,
+                        DataBroker, StepScan,
+                        DataMuxer)
