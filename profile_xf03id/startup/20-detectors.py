cec897be4a32b3411ad4b50e39846ff90022d3bd
ahaibof <bszy4593@163.com>
2021-05-28 11:32:19 +0800

    Cleanup detectors, use filestore

--- profile_xf03id/startup/20-detectors.py
+++ profile_xf03id/startup/20-detectors.py
@@ -2,12 +2,15 @@ from ophyd.controls import EpicsSignal, EpicsScaler
 from ophyd.controls import SignalDetector
 import pandas as pd
 
+from hxntools.detectors import (Xspress3Detector, TimepixDetector)
+from hxntools.detectors import Xspress3ROI
+from hxntools.detectors.zebra import HXNZebra
+
 # Fly scan ROIs to display after the scan
-#                   (Channel, (low bin in eV, high bin in eV))
-FLY_XSPRESS3_ROI = [(1, (9300, 9600)),
-                    (2, (9300, 9600)),
-                    (3, (9300, 9600)),
-                    (4, (9300, 9600)),
+FLY_XSPRESS3_ROI = [Xspress3ROI(chan=1, ev_low=9300, ev_high=9600),
+                    Xspress3ROI(chan=2, ev_low=9300, ev_high=9600),
+                    Xspress3ROI(chan=3, ev_low=9300, ev_high=9600),
+                    Xspress3ROI(chan=4, ev_low=9300, ev_high=9600),
                     ]
 
 # Scaler 1 MCA channels numbers to record with fly scans
@@ -18,9 +21,16 @@ FLY_SCALER1_CHANS = [2, 3, 8]
 pd.options.display.width = 180
 pd.options.display.max_rows = None
 
-timepix1 = TimepixDetector('XF:03IDC-ES{Tpx:1}', files=['TIFF1:'])
-timepix2 = TimepixDetector('XF:03IDC-ES{Tpx:2}', files=['TIFF1:'])
-xspress3 = Xspress3Detector('XF:03IDC-ES{Xsp:1}:', cam='', files=['HDF5:'])
+timepix1 = TimepixDetector('XF:03IDC-ES{Tpx:1}', files=['TIFF1:'], name='timepix1',
+                           file_path='/data', ioc_file_path='/data')
+timepix2 = TimepixDetector('XF:03IDC-ES{Tpx:2}', files=['TIFF1:'], name='timepix2',
+                           file_path='/data', ioc_file_path='/data')
+xspress3 = Xspress3Detector('XF:03IDC-ES{Xsp:1}:', cam='', files=['HDF5:'],
+                            name='xspress3',
+                            file_path='/xspress3_data/',
+                            ioc_file_path='/xspress3_data/')
+
+zebra = HXNZebra('XF:03IDC-ES{Zeb:1}:', name='zebra1')
 
 # 3IDC RG:C4 VME scalers
 sclr1 = EpicsScaler('XF:03IDC-ES{Sclr:1}', name='sclr1')
@@ -35,9 +45,13 @@ n_scaler_mca = 8
 sclr1_mca = [EpicsSignal('XF:03IDC-ES{Sclr:1}Mca:%d' % (i, ))
              for i in range(1, n_scaler_mca + 1)]
 
-sclr_trig = EpicsSignal('XF:03IDC-ES{Sclr:2}.CNT', rw=True, name='sclr_trig')
+sclr1_trig = EpicsSignal('XF:03IDC-ES{Sclr:1}.CNT', rw=True, name='sclr1_trig')
+det_sclr1 = SignalDetector()
+det_sclr1.add_acquire_signal(sclr1_trig)
+
+sclr2_trig = EpicsSignal('XF:03IDC-ES{Sclr:2}.CNT', rw=True, name='sclr2_trig')
 det_sclr2 = SignalDetector()
-det_sclr2.add_acquire_signal(sclr_trig)
+det_sclr2.add_acquire_signal(sclr2_trig)
 
 # Ion chamber
 ion0 = EpicsSignal('XF:03IDC-ES{Sclr:2}_cts1.B', name='ion0')
@@ -74,41 +88,7 @@ Pt_ch1 = EpicsSignal('XF:03IDC-ES{Xsp:1}:C1_ROI1:Value_RBV', name='Pt_ch1')
 Pt_ch2 = EpicsSignal('XF:03IDC-ES{Xsp:1}:C2_ROI1:Value_RBV', name='Pt_ch2')
 Pt_ch3 = EpicsSignal('XF:03IDC-ES{Xsp:1}:C3_ROI1:Value_RBV', name='Pt_ch3')
 
-xrf_erase = EpicsSignal('XF:03IDC-ES{Xsp:1}:ERASE', name='xrf_erase')
-xrf_acquire = EpicsSignal('XF:03IDC-ES{Xsp:1}:Acquire', name='xrf_acquire')
-xrf_save = EpicsSignal('XF:03IDC-ES{Xsp:1}:HDF5:Capture', rw=True, name='xrf_save')
-xrf_num_images = EpicsSignal('XF:03IDC-ES{Xsp:1}:NumImages', name='xrf_num_images')
-xrf_trig = EpicsSignal('XF:03IDC-ES{Xsp:1}:TRIGGER', name='xrf_trig')
-xrf_trig_mode = EpicsSignal('XF:03IDC-ES{Xsp:1}:TriggerMode', name='xrf_trig_mode')
-xrf_filename = EpicsSignal('XF:03IDC-ES{Xsp:1}:HDF5:FileName', name='xrf_filename')
-xrf_filepath = EpicsSignal('XF:03IDC-ES{Xsp:1}:HDF5:FilePath', name='xrf_filepath')
-xrf_filenumber = EpicsSignal('XF:03IDC-ES{Xsp:1}:HDF5:FileNumber', name='xrf_filenumber')
-
-# Timepix detector
-
-tpx1_filenumber = EpicsSignal('XF:03IDC-ES{Tpx:1}TIFF1:FileNumber', name='tpx1_filenumber')
-tpx1_acquire = EpicsSignal('XF:03IDC-ES{Tpx:1}cam1:Acquire', name='tpx1_acquire')
-tpx1_trig_mode = EpicsSignal('XF:03IDC-ES{Tpx:1}cam1:TriggerMode', name='tpx1_trig_mode')
-tpx1_filepath = EpicsSignal('XF:03IDC-ES{Tpx:1}TIFF1:FilePath', name='tpx1_filepath')
-tpx1_filename = EpicsSignal('XF:03IDC-ES{Tpx:1}TIFF1:FileName', name='tpx1_filename')
-tpx1_autosave = EpicsSignal('XF:03IDC-ES{Tpx:1}TIFF1:AutoSave', name='tpx1_autosave')
-
-det_tpx1_acquire = SignalDetector()
-det_tpx1_acquire.add_acquire_signal(tpx1_acquire)
-
-
-
 # Fine sample stage readbacks
 ssx_rbv = EpicsMotor('XF:03IDC-ES{Ppmac:1-ssx}Mtr.RBV', name='ssx_rbv')
 ssy_rbv = EpicsMotor('XF:03IDC-ES{Ppmac:1-ssy}Mtr.RBV', name='ssy_rbv')
 ssz_rbv = EpicsMotor('XF:03IDC-ES{Ppmac:1-ssz}Mtr.RBV', name='ssz_rbv')
-
-
-det_xrf_erase = SignalDetector()
-det_xrf_erase.add_acquire_signal(xrf_erase)
-det_xrf_trig = SignalDetector()
-det_xrf_trig.add_acquire_signal(xrf_trig)
-det_xrf_save = SignalDetector()
-det_xrf_save.add_acquire_signal(xrf_save)
-det_xrf_acquire = SignalDetector()
-det_xrf_acquire.add_acquire_signal(xrf_acquire)
