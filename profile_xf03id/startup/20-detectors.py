d53c1cded0ec191467829ba0e6f461812ce8ef38
ahaibof <bszy4593@163.com>
2021-07-09 00:37:06 +0800

    Add beam status detector

--- profile_xf03id/startup/20-detectors.py
+++ profile_xf03id/startup/20-detectors.py
@@ -2,7 +2,8 @@ from ophyd.controls import EpicsSignal, EpicsScaler
 from ophyd.controls import SignalDetector
 import pandas as pd
 
-from hxntools.detectors import (Xspress3Detector, TimepixDetector)
+from hxntools.detectors import (Xspress3Detector, TimepixDetector,
+                                MerlinDetector, BeamStatusDetector)
 from hxntools.detectors import Xspress3ROI
 from hxntools.detectors.zebra import HXNZebra
 
@@ -28,7 +29,7 @@ roi_elements = [(1340, 1640, 'Al'),
 FLY_XSPRESS3_ROI = []
 for ev_low, ev_high, name in roi_elements:
     for chan in range(1, 4):
-        roi = Xspress3ROI(chan=chan, ev_low=ev_low, ev_high=ev_high, 
+        roi = Xspress3ROI(chan=chan, ev_low=ev_low, ev_high=ev_high,
                           name='Det%d_%s' % (chan, name))
         FLY_XSPRESS3_ROI.append(roi)
 
@@ -36,19 +37,25 @@ for ev_low, ev_high, name in roi_elements:
 # (if above 8, be sure to modify n_scaler_mca below)
 FLY_SCALER1_CHANS = [2, 3, 8]
 
-# Flyscan results are shown using pandas. Maximum rows/columns to use when printing the table:
+# Flyscan results are shown using pandas. Maximum rows/columns to use when
+# printing the table:
 pd.options.display.width = 180
 pd.options.display.max_rows = None
-pd.options.display.max_columns = None
+pd.options.display.max_columns = 10
 
-timepix1 = TimepixDetector('XF:03IDC-ES{Tpx:1}', files=['TIFF1:'], name='timepix1',
+timepix1 = TimepixDetector('XF:03IDC-ES{Tpx:1}', files=['TIFF1:'],
+                           name='timepix1',
                            file_path='/data', ioc_file_path='/data')
-timepix2 = TimepixDetector('XF:03IDC-ES{Tpx:2}', files=['TIFF1:'], name='timepix2',
+timepix2 = TimepixDetector('XF:03IDC-ES{Tpx:2}', files=['TIFF1:'],
+                           name='timepix2',
                            file_path='/data', ioc_file_path='/data')
 xspress3 = Xspress3Detector('XF:03IDC-ES{Xsp:1}:', cam='', files=['HDF5:'],
                             name='xspress3',
                             file_path='/xspress3_data/',
                             ioc_file_path='/xspress3_data/')
+merlin1 = MerlinDetector('XF:03IDC-ES{Merlin:1}', files=['TIFF1:'],
+                         name='merlin1',
+                         file_path='/data', ioc_file_path='/data')
 
 zebra = HXNZebra('XF:03IDC-ES{Zeb:1}:', name='zebra1')
 
@@ -56,10 +63,14 @@ zebra = HXNZebra('XF:03IDC-ES{Zeb:1}:', name='zebra1')
 sclr1 = EpicsScaler('XF:03IDC-ES{Sclr:1}', name='sclr1')
 sclr2 = EpicsScaler('XF:03IDC-ES{Sclr:2}', name='sclr2')
 
-sclr1_erase_start = EpicsSignal('XF:03IDC-ES{Sclr:1}EraseStart', rw=True, name='sclr1_erase_start')
-sclr1_nuse_all = EpicsSignal('XF:03IDC-ES{Sclr:1}NuseAll', rw=True, name='sclr1_nuse_all')
-sclr1_channel_advance = EpicsSignal('XF:03IDC-ES{Sclr:1}ChannelAdvance', rw=True, name='sclr1_channel_advance')
-sclr1_input_mode = EpicsSignal('XF:03IDC-ES{Sclr:1}InputMode', rw=True, name='sclr1_input_mode')
+sclr1_erase_start = EpicsSignal('XF:03IDC-ES{Sclr:1}EraseStart', rw=True,
+                                name='sclr1_erase_start')
+sclr1_nuse_all = EpicsSignal('XF:03IDC-ES{Sclr:1}NuseAll', rw=True,
+                             name='sclr1_nuse_all')
+sclr1_channel_advance = EpicsSignal('XF:03IDC-ES{Sclr:1}ChannelAdvance',
+                                    rw=True, name='sclr1_channel_advance')
+sclr1_input_mode = EpicsSignal('XF:03IDC-ES{Sclr:1}InputMode', rw=True,
+                               name='sclr1_input_mode')
 
 n_scaler_mca = 8
 sclr1_mca = [EpicsSignal('XF:03IDC-ES{Sclr:1}Mca:%d' % (i, ))
@@ -76,12 +87,14 @@ det_sclr2.add_acquire_signal(sclr2_trig)
 # Ion chamber
 sclr2_ch2 = EpicsSignal('XF:03IDC-ES{Sclr:2}_cts1.B', name='sclr2_ch2')
 sclr2_ch3 = EpicsSignal('XF:03IDC-ES{Sclr:2}_cts1.C', name='sclr2_ch3')
-sclr2_ch4_calc = EpicsSignal('XF:03IDC-ES{Sclr:2}_calc4.VAL', name='sclr2_ch4_calc')
+sclr2_ch4_calc = EpicsSignal('XF:03IDC-ES{Sclr:2}_calc4.VAL',
+                             name='sclr2_ch4_calc')
 sclr2_ch4 = EpicsSignal('XF:03IDC-ES{Sclr:2}_cts1.D', name='sclr2_ch4')
 
 sclr1_ch2 = EpicsSignal('XF:03IDC-ES{Sclr:2}_cts1.B', name='sclr1_ch2')
 sclr1_ch3 = EpicsSignal('XF:03IDC-ES{Sclr:2}_cts1.C', name='sclr1_ch3')
-sclr1_ch4_calc = EpicsSignal('XF:03IDC-ES{Sclr:2}_calc4.VAL', name='sclr1_ch4_calc')
+sclr1_ch4_calc = EpicsSignal('XF:03IDC-ES{Sclr:2}_calc4.VAL',
+                             name='sclr1_ch4_calc')
 sclr1_ch4 = EpicsSignal('XF:03IDC-ES{Sclr:2}_cts1.D', name='sclr1_ch4')
 
 t_base = EpicsSignal('XF:03IDC-ES{LS:2-Ch:D}C:T-I', name='t_base')
@@ -181,3 +194,8 @@ det3_Pt = EpicsSignal('XF:03IDC-ES{Xsp:1}:C3_ROI1:Value_RBV', name='Det3_Pt')
 ssx_rbv = EpicsMotor('XF:03IDC-ES{Ppmac:1-ssx}Mtr.RBV', name='ssx_rbv')
 ssy_rbv = EpicsMotor('XF:03IDC-ES{Ppmac:1-ssy}Mtr.RBV', name='ssy_rbv')
 ssz_rbv = EpicsMotor('XF:03IDC-ES{Ppmac:1-ssz}Mtr.RBV', name='ssz_rbv')
+
+sr_shutter_status = EpicsSignal('SR-EPS{PLC:1}Sts:MstrSh-Sts', rw=False)
+sr_beam_current = EpicsSignal('SR:C03-BI{DCCT:1}I:Real-I', rw=False)
+
+det_beamstatus = BeamStatusDetector(min_current=100.0)
