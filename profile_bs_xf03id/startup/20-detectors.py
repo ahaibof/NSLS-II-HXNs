12ede7f1f489920b18522488f9217335682d0192
ahaibof <bszy4593@163.com>
2021-12-03 00:22:30 +0800

    Backport updated interferometer settings

--- profile_bs_xf03id/startup/20-detectors.py
+++ profile_bs_xf03id/startup/20-detectors.py
@@ -75,8 +75,27 @@ sigy = EpicsSignal('XF:03IDB-BI{Xeye-CAM:1}Stats1:SigmaY_RBV', name='sigy')
 int_sx = EpicsSignal('XF:03IDC-ES{FPS:1-Chan0}Pos-I', name='int_sx')
 int_sy = EpicsSignal('XF:03IDC-ES{FPS:1-Chan1}Pos-I', name='int_sy')
 int_sz = EpicsSignal('XF:03IDC-ES{FPS:1-Chan2}Pos-I', name='int_sz')
+
 int_hx = EpicsSignal('XF:03IDC-ES{FPS:2-Chan0}Pos-I', name='int_hx')
 int_vy = EpicsSignal('XF:03IDC-ES{FPS:2-Chan1}Pos-I', name='int_vy')
+int_hy = EpicsSignal('XF:03IDC-ES{FPS:2-Chan2}Pos-I', name='int_hy')
+
+int_hz = EpicsSignal('XF:03IDC-ES{FPS:3-Chan0}Pos-I', name='int_hz')
+int_vx = EpicsSignal('XF:03IDC-ES{FPS:3-Chan1}Pos-I', name='int_vx')
+int_vz = EpicsSignal('XF:03IDC-ES{FPS:3-Chan2}Pos-I', name='int_vz')
+
+int_zpssx = EpicsSignal('XF:03IDC-ES{FPS:4-Chan0}Pos-I', name='int_zpssx')
+int_zpssy = EpicsSignal('XF:03IDC-ES{FPS:4-Chan1}Pos-I', name='int_zpssy')
+int_zpssz = EpicsSignal('XF:03IDC-ES{FPS:4-Chan2}Pos-I', name='int_zpssz')
+
+int_zpx1 = EpicsSignal('XF:03IDC-ES{FPS:5-Chan0}Pos-I', name='int_zpx1')
+int_zpx2 = EpicsSignal('XF:03IDC-ES{FPS:5-Chan1}Pos-I', name='int_zpx2')
+int_zpy1 = EpicsSignal('XF:03IDC-ES{FPS:5-Chan2}Pos-I', name='int_zpy1')
+
+int_zpy2 = EpicsSignal('XF:03IDC-ES{FPS:6-Chan0}Pos-I', name='int_zpy2')
+int_zpz = EpicsSignal('XF:03IDC-ES{FPS:6-Chan1}Pos-I', name='int_zpz')
+int_zpspare1 = EpicsSignal('XF:03IDC-ES{FPS:6-Chan2}Pos-I',
+                           name='int_zpspare1')
 
 # Front-end Xray BPMs and local bumps
 xbpm_x = EpicsSignal('SR:C03-BI{XBPM:1}Pos:X-I', name='xbpm_x')
@@ -104,19 +123,6 @@ slit1_ypos = EpicsSignal('XF:03IDA-BI{Slt:1}PosY-I', name='slit1_ypos')
 #dcm_p = EpicsSignal('XF:03IDA-OP{Mon:1-Ax:P}Mtr.RBV', name='dcm_p')
 
 #tpx1_roi = EpicsSignal('XF:03IDC-ES{Tpx:1}Stats1:Total_RBV', name='tpx1_roi')
-
-
-# Unused interferometer signals
-# int_2ch2 = EpicsSignal('XF:03IDC-ES{FPS:2-Chan2}Pos-I', name='int_2ch2')
-# int_3ch0 = EpicsSignal('XF:03IDC-ES{FPS:3-Chan0}Pos-I', name='int_3ch0')
-# int_3ch1 = EpicsSignal('XF:03IDC-ES{FPS:3-Chan1}Pos-I', name='int_3ch1')
-# int_3ch2 = EpicsSignal('XF:03IDC-ES{FPS:3-Chan2}Pos-I', name='int_3ch2')
-# int_4ch0 = EpicsSignal('XF:03IDC-ES{FPS:4-Chan0}Pos-I', name='int_4ch0')
-# int_4ch1 = EpicsSignal('XF:03IDC-ES{FPS:4-Chan1}Pos-I', name='int_4ch1')
-# int_4ch2 = EpicsSignal('XF:03IDC-ES{FPS:4-Chan2}Pos-I', name='int_4ch2')
-# int_5ch0 = EpicsSignal('XF:03IDC-ES{FPS:5-Chan0}Pos-I', name='int_5ch0')
-# int_5ch1 = EpicsSignal('XF:03IDC-ES{FPS:5-Chan1}Pos-I', name='int_5ch1')
-# int_5ch2 = EpicsSignal('XF:03IDC-ES{FPS:5-Chan2}Pos-I', name='int_5ch2')
 
 sr_shutter_status = EpicsSignal('SR-EPS{PLC:1}Sts:MstrSh-Sts', rw=False)
 sr_beam_current = EpicsSignal('SR:C03-BI{DCCT:1}I:Real-I', rw=False)
