a36bc6be2e3646a444038765c9a08591d322b32c
ahaibof <bszy4593@163.com>
2022-01-17 15:52:31 +0800

    Machine PV positioner

--- profile_bs_xf03id/startup/11-machine.py
+++ profile_bs_xf03id/startup/11-machine.py
@@ -1,40 +1,24 @@
-from ophyd.controls import PVPositioner
+from ophyd import (PVPositioner, Component as Cpt,
+                   EpicsSignal, EpicsSignalRO)
 
 # Undulator
 
-ugap = PVPositioner('SR:C3-ID:G1{IVU20:1-Mtr:2}Inp:Pos',
-                    readback='SR:C3-ID:G1{IVU20:1-Mtr:2}Pos.RBV',
-                    act='SR:C3-ID:G1{IVU20:1-Mtr:2}Sw:Go', act_val=1,
-                    stop='SR:C3-ID:G1{IVU20:1-Mtr:2}Pos.STOP', stop_val=1,
-                    done='SR:C3-ID:G1{IVU20:1-Mtr:2}Pos.DMOV', done_val=1,
-                    put_complete=False,
-                    settle_time=1.5,
-                    name='ugap')
+class UgapPositioner(PVPositioner):
+    setpoint = Cpt(EpicsSignal, 'SR:C3-ID:G1{IVU20:1-Mtr:2}Inp:Pos')
+    readback = Cpt(EpicsSignalRO, 'SR:C3-ID:G1{IVU20:1-Mtr:2}Pos.RBV')
+    actuate = Cpt(EpicsSignal, 'SR:C3-ID:G1{IVU20:1-Mtr:2}Sw:Go')
+
+    actuate_value = 1
+    stop_signal = Cpt(EpicsSignal, 'SR:C3-ID:G1{IVU20:1-Mtr:2}Pos.STOP')
+
+    stop_value = 1
+    done = Cpt(EpicsSignalRO, 'SR:C3-ID:G1{IVU20:1-Mtr:2}Pos.DMOV')
+
+
+ugap = UgapPositioner(prefix='', settle_time=1.5, name='ugap')
 
 # Front End Slits (Primary Slits)
-
-fe_tb = PVPositioner('FE:C03A-OP{Slt:1-Ax:T}Mtr.VAL',
-                     readback='FE:C03A-OP{Slt:1-Ax:T}Mtr.RBV',
-                     stop='FE:C03A-OP{Slt:1-Ax:T}Mtr.STOP',
-                     stop_val=1, put_complete=True,
-                     name='fe_tb')
-
-fe_bb = PVPositioner('FE:C03A-OP{Slt:2-Ax:B}Mtr.VAL',
-                     readback='FE:C03A-OP{Slt:2-Ax:B}Mtr.RBV',
-                     stop='FE:C03A-OP{Slt:2-Ax:B}Mtr.STOP',
-                     stop_val=1, put_complete=True,
-                     name='fe_bb')
-
-fe_ib = PVPositioner('FE:C03A-OP{Slt:2-Ax:I}Mtr.VAL',
-                     readback='FE:C03A-OP{Slt:2-Ax:I}Mtr.RBV',
-                     stop='FE:C03A-OP{Slt:2-Ax:I}Mtr.STOP',
-                     stop_val=1, put_complete=True,
-                     name='fe_ib')
-
-fe_ob = PVPositioner('FE:C03A-OP{Slt:1-Ax:O}Mtr.VAL',
-                     readback='FE:C03A-OP{Slt:1-Ax:O}Mtr.RBV',
-                     stop='FE:C03A-OP{Slt:1-Ax:O}Mtr.STOP',
-                     stop_val=1, put_complete=True,
-                     name='fe_ob')
-
-
+fe_tb = EpicsMotor('FE:C03A-OP{Slt:1-Ax:T}Mtr.VAL', name='fe_tb')
+fe_bb = EpicsMotor('FE:C03A-OP{Slt:2-Ax:B}Mtr.VAL', name='fe_bb')
+fe_ib = EpicsMotor('FE:C03A-OP{Slt:2-Ax:I}Mtr.VAL', name='fe_ib')
+fe_ob = EpicsMotor('FE:C03A-OP{Slt:1-Ax:O}Mtr.VAL', name='fe_ob')
