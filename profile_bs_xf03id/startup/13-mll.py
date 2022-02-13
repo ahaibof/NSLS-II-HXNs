85bfca008f87232e74d28742cbd036a61c96ea00
ahaibof <bszy4593@163.com>
2022-02-13 13:15:32 +0800

    MLL device cleanup

--- profile_bs_xf03id/startup/13-mll.py
+++ profile_bs_xf03id/startup/13-mll.py
@@ -1,48 +1,125 @@
-from ophyd import (EpicsMotor, PVPositioner, PseudoPositioner)
+from ophyd import (EpicsMotor, PVPositioner, PseudoPositioner,
+                   Device, Component as Cpt)
 
-ssx = EpicsMotor('XF:03IDC-ES{Ppmac:1-ssx}Mtr', name='ssx')
-ssy = EpicsMotor('XF:03IDC-ES{Ppmac:1-ssy}Mtr', name='ssy')
-ssz = EpicsMotor('XF:03IDC-ES{Ppmac:1-ssz}Mtr', name='ssz')
 
-sth = EpicsMotor('XF:03IDC-ES{ANC350:1-Ax:0}Mtr', name='sth')
-# anc1m1 = EpicsMotor('XF:03IDC-ES{ANC350:1-Ax:1}Mtr', name='anc1m1')
-hth = EpicsMotor('XF:03IDC-ES{ANC350:1-Ax:2}Mtr', name='hth')
-# anc350_ssx = EpicsMotor('XF:03IDC-ES{ANC350:1-Ax:3}Mtr', name='anc350_ssx')
-# anc350_ssy = EpicsMotor('XF:03IDC-ES{ANC350:1-Ax:4}Mtr', name='anc350_ssy')
-# anc350_ssz = EpicsMotor('XF:03IDC-ES{ANC350:1-Ax:5}Mtr', name='anc350_ssz')
+def rename_motors(device):
+    cls = device.__class__
 
-vx = EpicsMotor('XF:03IDC-ES{ANC350:2-Ax:0}Mtr', name='vx')
-vy = EpicsMotor('XF:03IDC-ES{ANC350:2-Ax:1}Mtr', name='vy')
-vz = EpicsMotor('XF:03IDC-ES{ANC350:2-Ax:2}Mtr', name='vz')
-vchi = EpicsMotor('XF:03IDC-ES{ANC350:2-Ax:4}Mtr', name='vchi')
-vth = EpicsMotor('XF:03IDC-ES{ANC350:2-Ax:3}Mtr', name='vth')
-hx = EpicsMotor('XF:03IDC-ES{ANC350:2-Ax:5}Mtr', name='hx')
-sy = EpicsMotor('XF:03IDC-ES{ANC350:3-Ax:0}Mtr', name='sy')
-sx1 = EpicsMotor('XF:03IDC-ES{ANC350:3-Ax:1}Mtr', name='sx1')
-sz = EpicsMotor('XF:03IDC-ES{ANC350:3-Ax:2}Mtr', name='sz')
-sz1 = EpicsMotor('XF:03IDC-ES{ANC350:3-Ax:3}Mtr', name='sz1')
-hy = EpicsMotor('XF:03IDC-ES{ANC350:4-Ax:0}Mtr', name='hy')
-hz = EpicsMotor('XF:03IDC-ES{ANC350:4-Ax:1}Mtr', name='hz')
-osax = EpicsMotor('XF:03IDC-ES{ANC350:4-Ax:2}Mtr', name='osax')
-osay = EpicsMotor('XF:03IDC-ES{ANC350:4-Ax:3}Mtr', name='osay')
-osaz = EpicsMotor('XF:03IDC-ES{ANC350:4-Ax:4}Mtr', name='osaz')
-sx = EpicsMotor('XF:03IDC-ES{ANC350:4-Ax:5}Mtr', name='sx')
-bsx = EpicsMotor('XF:03IDC-ES{ANC350:5-Ax:0}Mtr', name='bsx')
-bsy = EpicsMotor('XF:03IDC-ES{ANC350:5-Ax:1}Mtr', name='bsy')
+    for attribute in device.signal_names:
+        motor = getattr(device, attribute)
+        if isinstance(motor, EpicsMotor):
+            component = getattr(cls, attribute)
+            motor.name = component.kwargs['name']
 
-# anc5m2 = EpicsMotor('XF:03IDC-ES{ANC350:5-Ax:2}Mtr', name='anc5m2')
-# anc5m3 = EpicsMotor('XF:03IDC-ES{ANC350:5-Ax:3}Mtr', name='anc5m3')
-# anc5m4 = EpicsMotor('XF:03IDC-ES{ANC350:5-Ax:4}Mtr', name='anc5m4')
-# anc5m5 = EpicsMotor('XF:03IDC-ES{ANC350:5-Ax:5}Mtr', name='anc5m5')
-# anc6m0 = EpicsMotor('XF:03IDC-ES{ANC350:6-Ax:0}Mtr', name='anc6m0')
-# anc6m1 = EpicsMotor('XF:03IDC-ES{ANC350:6-Ax:1}Mtr', name='anc6m1')
-# anc6m2 = EpicsMotor('XF:03IDC-ES{ANC350:6-Ax:2}Mtr', name='anc6m2')
-# anc6m3 = EpicsMotor('XF:03IDC-ES{ANC350:6-Ax:3}Mtr', name='anc6m3')
-# anc6m4 = EpicsMotor('XF:03IDC-ES{ANC350:6-Ax:4}Mtr', name='anc6m4')
-# anc6m5 = EpicsMotor('XF:03IDC-ES{ANC350:6-Ax:5}Mtr', name='anc6m5')
-# anc7m0 = EpicsMotor('XF:03IDC-ES{ANC350:7-Ax:0}Mtr', name='anc7m0')
-# anc7m1 = EpicsMotor('XF:03IDC-ES{ANC350:7-Ax:1}Mtr', name='anc7m1')
-# anc7m2 = EpicsMotor('XF:03IDC-ES{ANC350:7-Ax:2}Mtr', name='anc7m2')
+
+class HxnMLLSample(Device):
+    fine_x = Cpt(EpicsMotor, 'XF:03IDC-ES{Ppmac:1-ssx}Mtr', name='ssx')
+    fine_y = Cpt(EpicsMotor, 'XF:03IDC-ES{Ppmac:1-ssy}Mtr', name='ssy')
+    fine_z = Cpt(EpicsMotor, 'XF:03IDC-ES{Ppmac:1-ssz}Mtr', name='ssz')
+    th = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:1-Ax:0}Mtr', name='th')
+
+    coarse_x = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:4-Ax:5}Mtr', name='sx')
+    coarse_y = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:3-Ax:0}Mtr', name='sy')
+    coarse_x1 = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:3-Ax:1}Mtr', name='sx1')
+    coarse_z = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:3-Ax:2}Mtr', name='sz')
+    coarse_z1 = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:3-Ax:3}Mtr', name='sz1')
+
+
+smll = HxnMLLSample('', name='smll')
+ssx = smll.fine_x
+ssy = smll.fine_y
+ssz = smll.fine_z
+sth = smll.th
+
+sx = smll.coarse_x
+sy = smll.coarse_y
+sx1 = smll.coarse_x1
+sz = smll.coarse_z
+sz1 = smll.coarse_z1
+
+
+# NOTE: normally, motors would be named smll_{attribute} - or smll_coarse_x for
+#       example. To quickly rename them to what the component line shows, use
+#       'rename_motors' on the device:
+rename_motors(smll)
+
+
+class HxnAnc350_3(Device):
+    '''3 axis ANC350'''
+    ax0 = Cpt(EpicsMotor, '-Ax:0}Mtr')
+    ax1 = Cpt(EpicsMotor, '-Ax:1}Mtr')
+    ax2 = Cpt(EpicsMotor, '-Ax:2}Mtr')
+
+
+class HxnAnc350_4(Device):
+    '''4 axis ANC350'''
+    ax0 = Cpt(EpicsMotor, '-Ax:0}Mtr')
+    ax1 = Cpt(EpicsMotor, '-Ax:1}Mtr')
+    ax2 = Cpt(EpicsMotor, '-Ax:2}Mtr')
+    ax3 = Cpt(EpicsMotor, '-Ax:3}Mtr')
+
+
+class HxnAnc350_6(Device):
+    '''6 axis ANC350'''
+    ax0 = Cpt(EpicsMotor, '-Ax:0}Mtr')
+    ax1 = Cpt(EpicsMotor, '-Ax:1}Mtr')
+    ax2 = Cpt(EpicsMotor, '-Ax:2}Mtr')
+    ax3 = Cpt(EpicsMotor, '-Ax:3}Mtr')
+    ax4 = Cpt(EpicsMotor, '-Ax:4}Mtr')
+    ax5 = Cpt(EpicsMotor, '-Ax:5}Mtr')
+
+
+# Note that different controllers have different axis counts:
+anc350_1 = HxnAnc350_6('XF:03IDC-ES{ANC350:1', name='anc350_1')
+anc350_2 = HxnAnc350_6('XF:03IDC-ES{ANC350:2', name='anc350_2')
+anc350_3 = HxnAnc350_4('XF:03IDC-ES{ANC350:3', name='anc350_3')
+anc350_4 = HxnAnc350_6('XF:03IDC-ES{ANC350:4', name='anc350_4')
+anc350_5 = HxnAnc350_6('XF:03IDC-ES{ANC350:5', name='anc350_5')
+anc350_6 = HxnAnc350_6('XF:03IDC-ES{ANC350:6', name='anc350_6')
+anc350_7 = HxnAnc350_3('XF:03IDC-ES{ANC350:7', name='anc350_7')
+
+
+class HxnVerticalMLL(Device):
+    x = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:2-Ax:0}Mtr', name='vx')
+    y = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:2-Ax:1}Mtr', name='vy')
+    z = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:2-Ax:2}Mtr', name='vz')
+    chi = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:2-Ax:4}Mtr', name='vchi')
+    th = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:2-Ax:3}Mtr', name='vth')
+
+
+vmll = HxnVerticalMLL('', name='vmll')
+rename_motors(vmll)
+
+
+class HxnHorizontalMLL(Device):
+    x = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:2-Ax:5}Mtr', name='hx')
+    y = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:4-Ax:0}Mtr', name='hy')
+    z = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:4-Ax:1}Mtr', name='hz')
+    th = EpicsMotor('XF:03IDC-ES{ANC350:1-Ax:2}Mtr', name='hth')
+
+
+hmll = HxnHorizontalMLL('', name='hmll')
+rename_motors(hmll)
+
+
+class HxnMLL_OSA(Device):
+    osax = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:4-Ax:2}Mtr', name='osax')
+    osay = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:4-Ax:3}Mtr', name='osay')
+    osaz = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:4-Ax:4}Mtr', name='osaz')
+
+
+mllosa = HxnMLL_OSA('', name='mllosa')
+rename_motors(mllosa)
+
+
+class HxnMLLBeamStop(Device):
+    bsx = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:5-Ax:0}Mtr', name='bsx')
+    bsy = Cpt(EpicsMotor, 'XF:03IDC-ES{ANC350:5-Ax:1}Mtr', name='bsy')
+
+
+mllbs = HxnMLLBeamStop('', name='mllbs')
+rename_motors(mllbs)
+
 
 _xz_angle = 15. * pi / 180.
 
