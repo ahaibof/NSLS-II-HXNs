54e82182845ac07a2b08be01a3ecc3ff0cbad248
ahaibof <bszy4593@163.com>
2022-03-10 17:50:58 +0800

    Logbook setup tweak

--- profile_bs_xf03id/startup/02-callbacks.py
+++ profile_bs_xf03id/startup/02-callbacks.py
@@ -1,3 +1,5 @@
+from functools import partial
+
 import ophyd
 from ophyd import EpicsSignal
 from ophyd.callbacks import UidPublish
@@ -6,13 +8,24 @@ from hxntools.scan_number import HxnScanNumberPrinter
 from hxntools.scan_status import HxnScanStatus
 
 from bluesky.global_state import get_gs
+from bluesky.callbacks.olog import logbook_cb_factory
 
+from pyOlog import SimpleOlogClient
+
+# Set up the logbook. This configures bluesky's summaries of
+# data acquisition (scan type, ID, etc.).
+
+LOGBOOKS = ['bs-testing']
+
+logbook = SimpleOlogClient()
 
 uid_signal = EpicsSignal('XF:03IDC-ES{BS-Scan}UID-I')
 uid_broadcaster = UidPublish(uid_signal)
 hxn_scan_status = HxnScanStatus('XF:03IDC-ES{Status}ScanRunning-I')
+cb = logbook_cb_factory(partial(logbook.log, logbooks=LOGBOOKS))
 
-gs = get_gs()
-gs.RE.subscribe('all', HxnScanNumberPrinter())
-gs.RE.subscribe('all', uid_broadcaster)
-gs.RE.subscribe('all', hxn_scan_status)
+RE = get_gs().RE
+RE.subscribe('all', HxnScanNumberPrinter())
+RE.subscribe('all', uid_broadcaster)
+RE.subscribe('all', hxn_scan_status)
+RE.subscribe('start', cb)
