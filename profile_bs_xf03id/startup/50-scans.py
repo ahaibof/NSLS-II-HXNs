0cb9f4ddda16a1119dc46cf2da6d58cd77c0ac73
ahaibof <bszy4593@163.com>
2022-04-13 08:16:42 +0800

    tweak detector list with new devices

--- profile_bs_xf03id/startup/50-scans.py
+++ profile_bs_xf03id/startup/50-scans.py
@@ -2,8 +2,6 @@
 # HXN step-scan configuration
 
 import hxntools.scans
-# from hxntools.spiral_scans import HxnFermatScan
-
 from bluesky.global_state import get_gs
 
 gs = get_gs()
@@ -16,19 +14,8 @@ spiral = hxntools.scans.relative_spiral
 mesh = hxntools.scans.absolute_mesh
 dmesh = hxntools.scans.relative_mesh
 
+gs.DETS = [zebra, sclr1, smll, lakeshore2, xbpm, dcm, angle_x, angle_y, s1]
 
-# Define how spiral scans should work
-# fermat = HxnFermatScan()
-
-
-# default_detectors = [sclr1, det_beamstatus, sclr1_ch2, sclr1_ch3, sclr1_ch4,
-#                      ssx_rbv, ssy_rbv, ssz_rbv, t_base, t_sample, t_vlens,
-#                      t_hlens]
-
-# gs.DETS = [zebra, master_sclr1, sclr1_ch2, sclr1_ch3, sclr1_ch4, ssx, ssy,
-#   ssz, t_base, t_sample, t_vlens, t_hlens, xbpm_x, xbpm_y, quad_x, quad_y,
-#   dcm_th, dcm_p, angle_x, angle_y, slit1_top, slit1_bottom, slit1_right,
-#   slit1_left, slit1_xpos, slit1_ypos]
 
 gs.TABLE_COLS = ['sclr2_ch2','sclr2_ch3', 'sclr2_ch4', 'ssx', 'ssy', 'ssz',
                  't_base', 't_sample', 't_vlens', 't_hlens']
