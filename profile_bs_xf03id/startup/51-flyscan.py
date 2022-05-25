630c7c6e25aec7b0391ec97ef97408b85eb3b40d
ahaibof <bszy4593@163.com>
2022-05-26 02:34:33 +0800

    Add fly-scan/step-scan combination

--- profile_bs_xf03id/startup/51-flyscan.py
+++ profile_bs_xf03id/startup/51-flyscan.py
@@ -1,6 +1,6 @@
 # vim: sw=4 ts=4 sts=4 expandtab smarttab
 # HXN fly-scan configuration
-from hxnfly.bs import (FlyPlan1D, FlyPlan2D, maybe_a_table)
+from hxnfly.bs import (FlyPlan1D, FlyPlan2D, FlyStep1D, maybe_a_table)
 from hxnfly.hxn_fly import (Fly1D_MLL, Fly1D_ZP, Fly2D_MLL, Fly2D_ZP)
 
 # These define which scans can be done by which class:
@@ -12,6 +12,9 @@ FlyPlan1D.scans = {frozenset({ssx}): Fly1D_MLL,
                    frozenset({zpssy}): Fly1D_ZP,
                    frozenset({zpssz}): Fly1D_ZP,
                    }
+
+# Mixed 1D fly/1D step scan, same as above:
+FlyStep1D.scans = FlyPlan1D.scans
 
 # 2D scans:
 FlyPlan2D.scans = {frozenset({ssx, ssy}): Fly2D_MLL,
@@ -59,3 +62,8 @@ fly2d = FlyPlan2D(usable_detectors=fly_scannable_detectors,
                   scaler_channels=[1, 2, 3, 4])
 fly2d.sub_factories = [maybe_a_table]
 fly2d.subs = [pt_plot, live_im_plot, ]
+
+flystep = FlyStep1D(usable_detectors=fly_scannable_detectors,
+                    scaler_channels=[1, 2, 3, 4])
+flystep.sub_factories = [maybe_a_table]
+flystep.subs = [pt_plot, live_im_plot, ]
