3ce8b82edb1cc94650bd253ecf390eb1d2be0d35
ahaibof <bszy4593@163.com>
2022-06-06 06:21:30 +0800

    Default log-level should be INFO for hxnfly/hxntools

--- profile_collection/startup/00-startup.py
+++ profile_collection/startup/00-startup.py
@@ -13,8 +13,8 @@ logging.getLogger('hxntools').addHandler(handler)
 logging.getLogger('hxnfly').addHandler(handler)
 logging.getLogger('ppmac').addHandler(handler)
 
-logging.getLogger('hxnfly').setLevel(logging.DEBUG)
-logging.getLogger('hxntools').setLevel(logging.DEBUG)
+logging.getLogger('hxnfly').setLevel(logging.INFO)
+logging.getLogger('hxntools').setLevel(logging.INFO)
 logging.getLogger('ppmac').setLevel(logging.INFO)
 
 import pandas as pd
