dcd52d4c5165065dd1c33733cfc23392724290a1
ahaibof <bszy4593@163.com>
2022-09-17 03:18:54 +0800

    Enable suspenders

--- profile_collection/startup/52-suspenders.py
+++ profile_collection/startup/52-suspenders.py
@@ -8,8 +8,8 @@ RE = gs.RE
 # Here are some conditions that will cause scans to pause automatically:
 # - when the beam current goes below a certain threshold
 susp_current = SuspendFloor(beamline_status.beam_current,
-                            suspend_thresh=100.0,
-                            resume_thresh=105.0,
+                            suspend_thresh=200.0,
+                            resume_thresh=240.0,
                             tripped_message='beam current too low',
                             )
 
@@ -25,6 +25,6 @@ susp_enabled = SuspendBoolLow(beamline_status.beamline_enabled,
 
 # NOTE: to enable or disable the suspenders, (un)comment the following:
 # Install all suspenders:
-# RE.install_suspender(susp_current)
-# RE.install_suspender(susp_shutter)
-# RE.install_suspender(susp_enabled)
+RE.install_suspender(susp_current)
+RE.install_suspender(susp_shutter)
+RE.install_suspender(susp_enabled)
